f3f640ca3f77ee93a18a8ee3fef2cc422d5a9a91
JeanNesi <jean.carlos_nesi@hotmail.com>
2023-03-02 13:23:55 -0300

    firs commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function sendMessage(e) {
+  e.preventDefault();
+
+  console.log(new FormData(e.target));
+
+  var i = 0;
+  var element = document.getElementById("messagesContainer");
+
+  const messages = [
+    { text: e.name, className: "myMessage" },
+    { text: "fdsgdsfg", className: "myMessage" },
+    { text: "sdfgsdfg", className: "userMessage" },
+    { text: "sdfgfgdshjghjkgfhk", className: "userMessage" },
+  ];
+
+  const interval = setInterval(() => {
+    if (i == messages.length) {
+      clearInterval(interval);
+      return;
+    }
+
+    let div = document.createElement("div");
+    div.classList.add(messages[i].className);
+    div.append(messages[i].text);
+    element.append(div);
+    console.log(i);
+
+    element.scrollTo(0, element.scrollHeight);
+    i += 1;
+  }, 1000);
+}
