7d557b837392148663520b22b874080f0ab4ee05
Jean Carlos Nesi <jean.carlos_nesi@hotmail.com>
2023-03-16 21:54:37 -0300

    .

--- script.js
+++ script.js
@@ -1,37 +1,87 @@
-function sendMessage(e) {
-  e.preventDefault();
+window.onload = () => {
+  const messages = JSON.parse(localStorage.getItem("messages"));
+  messages.forEach((element) => {
+    createMessage({
+      origin: element.origin,
+      message: element.message,
+      time: element.dateTime,
+      status: element.status,
+    });
+  });
+  getAdvice();
+};
+
+async function createMessage({ origin, message, time, status }) {
+  const messagesContainer = document.getElementById("messagesContainer");
+
+  const div = document.createElement("div");
+  const pMessage = document.createElement("p");
+  const pHour = document.createElement("p");
+
+  div.classList.add(origin);
+  pHour.classList.add("hour");
+
+  div.append(pMessage);
+  div.append(pHour);
+
+  pMessage.innerHTML =
+    status === "oldMessage" || origin === "myMessage"
+      ? message
+      : await getAdvice();
+
+  pHour.innerHTML =
+    time ??
+    new Date().toLocaleTimeString("pt-br", {
+      timeStyle: "short",
+    });
+
+  messagesContainer.append(div);
+  messagesContainer.scrollTo(0, messagesContainer.scrollHeight);
+  saveMessages();
+}
+
+function sendMessage(event) {
+  event.preventDefault();
 
   const input = document.getElementById("input");
   if (!input.value) return;
 
-  const element = document.getElementById("messagesContainer");
+  createMessage({ message: input.value, origin: "myMessage" });
 
-  const messages = [
-    "O que é verde e fica sentado na esquina? Um pepino esperto.",
-    "Por que os peixes ficam tão quietos quando você os olha? Porque eles estão em água.",
-    "O que o ovo disse para o outro ovo? Estou com casca de lado.",
-    "Qual é a fruta mais inteligente? O abacaxi, porque ele é demasiado esperto para ser pego.",
-    "O que acontece quando você joga uma pedra em um carro? Você terá que comprar um novo carro.",
-    "Por que os porcos não usam relógios? Porque eles preferem acordar e grunhir.",
-    "O que o elefante disse para o computador? Não entendo bytes.",
-  ];
-
-  const div = document.createElement("div");
-  const p = document.createElement("p");
-  div.classList.add("myMessage");
-  div.append(p);
-  p.append(input.value);
-  element.append(div);
   input.value = "";
 
   setTimeout(() => {
-    const div = document.createElement("div");
-    const p = document.createElement("p");
-    div.classList.add("userMessage");
-    div.append(p);
-    p.innerHTML = messages[Math.floor(Math.random() * messages.length)];
-    element.append(div);
-
-    element.scrollTo(0, element.scrollHeight);
+    createMessage({ message: "teste", origin: "userMessage" });
   }, 1000);
+}
+
+function saveMessages() {
+  const messagesContainer = Array.from(
+    document.getElementById("messagesContainer").children
+  );
+
+  let array = [];
+
+  messagesContainer.forEach((element) => {
+    const div = element.children;
+    array.push({
+      origin: element.className,
+      dateTime: div[1].innerText,
+      message: div[0].innerText,
+      status: "oldMessage",
+    });
+    localStorage.setItem("messages", JSON.stringify(array));
+  });
+}
+
+async function getAdvice() {
+  let adviceDate;
+  await fetch("https://api.adviceslip.com/advice")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      adviceDate = data;
+    });
+  return adviceDate.slip.advice;
 }
